15cd6c6ebb9d359e19acbec4a66e418c68e5c569
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-09-03 15:14:20 -0400

    trying to get playwright working
    not working, need to try playwright in a vacuum I think, and then I might
    have better luck

--- tests/index.js
+++ tests/index.js
@@ -1,21 +1,47 @@
+import { chromium } from 'playwright';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
 import ResourceLoader from '../dist/loader.modern';
+var browser;
 
-test('Config is read', () => {
-	const defaults = [
-		[
-			'js:test',
-			{
-				url: '/assets/product/url.js',
-			},
-		],
-	];
+const browserOn = async () => {};
 
+const defaults = [
+	[
+		'js:test',
+		{
+			url: '/assets/product/url.js',
+		},
+	],
+];
+
+test('Config is stored in resources', () => {
 	const rl = new ResourceLoader(defaults, {});
-	console.log(rl.resources);
-	assert.is(rl.init(), true);
+
+	assert.equal(
+		rl.resources,
+		new Map([['js:test', { url: '/assets/product/url.js' }]])
+	);
+
+	assert.equal(rl.configs, {});
+
+	assert.equal(rl.resources.get('js:test'), {
+		url: '/assets/product/url.js',
+	});
 });
 
+test('Resource is rendered', async () => {
+	const rl = new ResourceLoader(defaults, {});
+
+	browser = await chromium.launch();
+	const page = await browser.newPage();
+	await page.goto('http://localhost:8000');
+	rl.init();
+});
+
+const browserOff = async () => browser.close();
+
+browserOn();
 test.run();
+browserOff();
