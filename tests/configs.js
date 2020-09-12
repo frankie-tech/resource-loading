d8d516a64393b5c9433a2e763e40a0ce37523369
frankie <26461046+frankiekie@users.noreply.github.com>
2020-09-12 04:00:49 -0400

    moves big hairy config js to separate file

---
+++ tests/configs.js
@@ -0,0 +1,10 @@
+const ResourceOptionsArray = [
+	[
+		'js:jquery',
+		{
+			url:
+				'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+			callback: () => console.log('something happened'),
+		},
+	],
+];
